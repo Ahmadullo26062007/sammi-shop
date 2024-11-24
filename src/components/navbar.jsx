9ffaaa47a1cdf375a58d4b0cfb916e7e573ba8df
Ahmadullo26062007 <ahmadullo0039@gmail.com>
2024-11-24 11:16:12 +0500

    test

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,13 +4,23 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <header className="flex items-center px-4 md:px-12 py-2 justify-between fixed top-0 w-full z-50 shadow bg-white">
-        <Link href="/">
-            <Image src={"./logo.svg"} alt="logo" width={150} height={40} />
-        </Link>
+      <Link href="/">
+        <Image src={"./logo.svg"} alt="logo" width={150} height={40} />
+      </Link>
 
-        <div className="flex items-center space-x-2.5 text-sm">
-            <button className="button bg-blue-600 text-white border-transparent hover:border-blue-600 hover:bg-transparent hover:text-black">Log in</button>
-        </div>
+      <div className="flex items-center space-x-2.5 text-sm">
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+          <a className="mr-5 hover:text-gray-900">Home page</a>
+          <a className="mr-5 hover:text-gray-900">All products</a> 
+        </nav>
+        <button className="button bg-blue-600 text-white border-transparent hover:border-blue-600 hover:bg-transparent hover:text-black">
+          Log in
+        </button>
+
+        <button className="button border-blue-600 bg-transparent hover:border-transparent hover:bg-blue-600 hover:text-white">
+          Sign up
+        </button>
+      </div>
     </header>
   );
 };
